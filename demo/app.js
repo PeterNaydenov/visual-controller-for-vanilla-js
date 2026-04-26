9a824495feaeb3b7ca51d863659efddf6bd56afd
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-26 14:12:12 +0300

    Refactoring demo component

--- demo/app.js
+++ demo/app.js
@@ -1,52 +1,54 @@
 function Hello ( props ) {
-  var container = props.container
-  var data = props.data
-  var setupUpdates = props.setupUpdates
-  var dependencies = props.dependencies
+    var container = props.container
+    var data = props.data || {}
+    var setupUpdates = props.setupUpdates
+  
+    var message = data.greeting || 'Hello'
+    var count = 0
 
-  var message = data.greeting || 'Hello'
-  var count = 0
+    container.innerHTML = `
+      <div class="hello">
+                  <h2> ${message} </h2>
+                  <p> Count: ${count} </p>
+                  <button> Increment </button>
+            </div>
+      `
+    
+    let 
+        button = container.querySelector ( 'button' )
+      , counter = container.querySelector ( 'p' )
+      , title = container.querySelector ( 'h2' )
+      ;
+    
+    button.addEventListener ( 'click', () => {
+              count++
+              counter.textContent = 'Count: ' + count
+        })
 
-  var wrapper = document.createElement('div')
-  wrapper.className = 'hello'
+    setupUpdates ({
+                  changeMessage: ( newMsg ) => {
+                            message = newMsg
+                            title.textContent = message
+                      },
+                  increment: () => {
+                            count++
+                            counter.textContent = 'Count: ' + count
+                        },
+                  getCount: () => {
+                            return count
+                        }
+            })
+  } // Hello func.
 
-  var title = document.createElement('h2')
-  title.textContent = message
-  wrapper.appendChild(title)
 
-  var counter = document.createElement('p')
-  counter.textContent = 'Count: ' + count
-  wrapper.appendChild(counter)
 
-  var button = document.createElement('button')
-  button.textContent = 'Increment'
-  button.addEventListener('click', function() {
-    count++
-    counter.textContent = 'Count: ' + count
-  })
-  wrapper.appendChild(button)
+function destroyApp () {}
 
-  container.appendChild(wrapper)
 
-  setupUpdates({
-    changeMessage: function(newMsg) {
-      message = newMsg
-      title.textContent = message
-    },
-    increment: function() {
-      count++
-      counter.textContent = 'Count: ' + count
-    },
-    getCount: function() {
-      return count
-    }
-  })
-}
-
-function destroyApp() {
-}
 
 export default {
-  start: Hello,
-  destroy: destroyApp
-}
+          start: Hello,
+          destroy: destroyApp
+    }
+
+
