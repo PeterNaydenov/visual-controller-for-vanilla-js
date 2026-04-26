b4dade8b6fc0daaa03ad553936bf4371d69055ae
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-26 13:57:35 +0300

    Cleaning...

--- demo/main.js
+++ demo/main.js
@@ -1,20 +1,24 @@
 import VisualController from '/src/main.js'
 import Hello from '/demo/app.js'
 
-var html = VisualController({
+var html = VisualController ({
   utils: {
-    capitalize: function(str) {
-      return str.charAt(0).toUpperCase() + str.slice(1)
-    }
-  }
-})
+            capitalize: ( str ) => str.charAt(0).toUpperCase() + str.slice (1)
+        }
+  })
 
-var hasTextBlock = document.getElementById('hasText')
-var updateMsgBtn = document.getElementById('updateMsg')
-var incrementBtn = document.getElementById('increment')
-var getCountBtn = document.getElementById('getCount')
-var destroyBtn = document.getElementById('destroy')
-var resultTextBlock = document.getElementById('resultText')
+
+
+const 
+      hasTextBlock    = document.getElementById ( 'hasText'   )
+    , updateMsgBtn    = document.getElementById ( 'updateMsg' )
+    , incrementBtn    = document.getElementById ( 'increment' )
+    , getCountBtn     = document.getElementById ( 'getCount'  )
+    , destroyBtn      = document.getElementById ( 'destroy'   )
+    , resultTextBlock = document.getElementById ( 'resultText')
+    ;
+
+
 
 html.publish ( Hello, { greeting: 'Hi from Vanilla JS!' }, 'app' )
   .then ( updates => {
@@ -22,18 +26,19 @@ html.publish ( Hello, { greeting: 'Hi from Vanilla JS!' }, 'app' )
               hasTextBlock.textContent = html.has ( 'app' )
     })
 
+
+const app = html.getApp ( 'app' );
+
+
 updateMsgBtn.addEventListener ( 'click', () => {
-              let app = html.getApp('app')
               if ( app )  app.changeMessage ( `Updated at ${ new Date().toLocaleTimeString()}` )
     })
 
 incrementBtn.addEventListener ( 'click', () => {
-              let app = html.getApp('app')
-              if ( app )  app.increment()
+              if ( app )  app.increment ()
     })
 
 getCountBtn.addEventListener('click', () => {
-              let app = html.getApp ( 'app' )
               if ( app )  resultTextBlock.textContent = app.getCount ()
     })
 
@@ -41,6 +46,6 @@ getCountBtn.addEventListener('click', () => {
 
 destroyBtn.addEventListener ( 'click', () => {
               const result = html.destroy ( 'app' )
-              resultTextBlock.textContent = 'Destroyed: ' + result
+              resultTextBlock.textContent = `Destroyed: ${result}`
               hasTextBlock.textContent = html.has ( 'app' )
     })
