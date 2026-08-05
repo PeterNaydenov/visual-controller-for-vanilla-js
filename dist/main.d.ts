f9eb76cb79f880581db74673a21fdf4cef2fa603
Peter Naydenov <peter.naydenov@gmail.com>
2026-08-05 18:01:16 +0300

    Types for v.2

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,31 +1,53 @@
 export default VisualController;
 /**
- * App definition structure
+ * Callback that places dim markers into the DOM.
  */
-export type AppDefinition = {
-    /**
-     * - Start function that receives props
-     */
-    start: Function;
-    /**
-     * - Optional destroy function for cleanup
-     */
-    destroy?: Function;
+export type SetCallback = (markers: {
+    start: Text;
+    end: Text;
+}) => string | void;
+/**
+ * Object passed to `setupUpdates` from inside a published app.
+ */
+export type SetupUpdates = any;
+/**
+ * Controller instance returned by `VisualController`.
+ */
+export type VisualControllerInstance = {
+    set: SetCallback & ((fn: SetCallback, ...args: any[]) => void);
+    publish: (alias: string, appDef: any, data?: object, extraParams?: object) => Promise<SetupUpdates | false>;
+    destroy: (target?: string | string[]) => boolean | number;
+    has: (alias: string) => boolean;
+    getApp: (alias: string) => SetupUpdates | false;
+    isEmpty: (alias: string) => boolean | undefined;
+    list: () => string[];
+    reset: () => void;
 };
 /**
- * Visual Controller for Vanilla JS
- * @param {Object} [dependencies={}] - Dependencies that should be available for all apps
- * @returns {Object} - Object with methods: publish, destroy, getApp, has
+ *  Callback that places dim markers into the DOM.
+ *  @callback SetCallback
+ *  @param {{ start: Text, end: Text }} markers
+ *  @returns {string | void}
  */
 /**
- * App definition structure
- * @typedef {Object} AppDefinition
- * @property {Function} start - Start function that receives props
- * @property {Function} [destroy] - Optional destroy function for cleanup
+ *  Object passed to `setupUpdates` from inside a published app.
+ *  @typedef {Object} SetupUpdates
  */
-declare function VisualController(dependencies?: {}): {
-    publish: (appDefinition: AppDefinition, data: any, id: string) => any;
-    destroy: (id: string) => boolean;
-    getApp: (id: string) => any | false;
-    has: (id: string) => boolean;
-};
+/**
+ *  Controller instance returned by `VisualController`.
+ *  @typedef {Object} VisualControllerInstance
+ *  @property {SetCallback & ((fn: SetCallback, ...args: any[]) => void)} set
+ *  @property {(alias: string, appDef: any, data?: object, extraParams?: object) => Promise<SetupUpdates | false>} publish
+ *  @property {(target?: string | string[]) => boolean | number} destroy
+ *  @property {(alias: string) => boolean} has
+ *  @property {(alias: string) => SetupUpdates | false} getApp
+ *  @property {(alias: string) => boolean | undefined} isEmpty
+ *  @property {() => string[]} list
+ *  @property {() => void} reset
+ */
+/**
+ *  Visual Controller for Vanilla JS
+ *  @param {Object} [dependencies={}]
+ *  @returns {VisualControllerInstance}
+ */
+declare function VisualController(dependencies?: any): VisualControllerInstance;
